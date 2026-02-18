fd2158c636b78e1b0fefd1fc81e23e01356ce8b1
alexa <alexa@DECXCED>
2026-02-18 19:29:42 +0200

    Add project files.

---
+++ Frags/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Frags.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
