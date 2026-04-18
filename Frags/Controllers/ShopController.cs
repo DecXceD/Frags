44a5ea35e9b40434575b091534751ad90b0aee8c
DecXceD <alexanderisaev@abv.bg>
2026-04-18 18:44:17 +0300

    Implement search, filtering and pagination in Shop

--- Frags/Controllers/ShopController.cs
+++ Frags/Controllers/ShopController.cs
@@ -13,12 +13,52 @@ namespace Frags.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(
+    string search,
+    int? brandId,
+    int? categoryId,
+    string gender,
+    int page = 1)
         {
-            var fragrances = await _context.Fragrances
+            int pageSize = 8;
+
+            var query = _context.Fragrances
                 .Include(f => f.Brand)
                 .Include(f => f.Category)
+                .AsQueryable();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(f => f.Name.Contains(search));
+            }
+
+            if (brandId.HasValue)
+            {
+                query = query.Where(f => f.BrandId == brandId);
+            }
+
+            if (categoryId.HasValue)
+            {
+                query = query.Where(f => f.CategoryId == categoryId);
+            }
+
+            if (!string.IsNullOrEmpty(gender))
+            {
+                query = query.Where(f => f.Gender == gender);
+            }
+
+            int totalItems = await query.CountAsync();
+
+            var fragrances = await query
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
                 .ToListAsync();
+
+            ViewBag.Brands = await _context.Brands.ToListAsync();
+            ViewBag.Categories = await _context.Categories.ToListAsync();
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = (int)Math.Ceiling((double)totalItems / pageSize);
 
             return View(fragrances);
         }
