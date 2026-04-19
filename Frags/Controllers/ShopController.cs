81a8ed4cb1070dea3f27cfabcd3d82adb256687a
DecXceD <alexanderisaev@abv.bg>
2026-04-19 10:06:14 +0300

    Enhance Shop UX with delayed auto-search after typing, filter dropdown auto-submit and improved pagination

--- Frags/Controllers/ShopController.cs
+++ Frags/Controllers/ShopController.cs
@@ -14,11 +14,12 @@ namespace Frags.Controllers
         }
 
         public async Task<IActionResult> Index(
-    string search,
-    int? brandId,
-    int? categoryId,
-    string gender,
-    int page = 1)
+        string search,
+        int? brandId,
+        int? categoryId,
+        string gender,
+        string sort,
+        int page = 1)
         {
             int pageSize = 8;
 
@@ -47,7 +48,25 @@ namespace Frags.Controllers
                 query = query.Where(f => f.Gender == gender);
             }
 
+            switch (sort)
+            {
+                case "price_asc":
+                    query = query.OrderBy(f => f.Price);
+                    break;
+                case "price_desc":
+                    query = query.OrderByDescending(f => f.Price);
+                    break;
+                case "name":
+                    query = query.OrderBy(f => f.Name);
+                    break;
+                default:
+                    query = query.OrderBy(f => f.Id);
+                    break;
+            }
+
             int totalItems = await query.CountAsync();
+
+            ViewBag.TotalItems = totalItems;
 
             var fragrances = await query
                 .Skip((page - 1) * pageSize)
