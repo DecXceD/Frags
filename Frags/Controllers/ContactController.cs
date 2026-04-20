6801a35852b0cd461b609d30bdaa1c11d4195321
DecXceD <alexanderisaev@abv.bg>
2026-04-20 13:22:24 +0300

    Add Contact controller and views

---
+++ Frags/Controllers/ContactController.cs
@@ -0,0 +1,58 @@
+﻿using Frags.Services.Interfaces;
+using Frags.Services.ViewModels.Contact;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Frags.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService service;
+
+        public ContactController(IContactService service)
+        {
+            this.service = service;
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            var contact = await service.GetAsync();
+
+            if (contact == null)
+                return NotFound();
+
+            return View(contact);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit()
+        {
+            var contact = await service.GetAsync();
+
+            if (contact == null)
+                return NotFound();
+
+            var model = new ContactFormModel
+            {
+                Id = contact.Id,
+                Email = contact.Email,
+                Phone = contact.Phone
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(ContactFormModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            await service.UpdateAsync(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
